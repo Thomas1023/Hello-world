19a0100cd0550888940baaa8d274ae1c46d39ac1
bowlans <thomas.steiner03@gmail.com>
2018-10-12 17:38:57 -0700

    face ai
    nice and quant

--- Java/FaceDetection.java
+++ Java/FaceDetection.java
@@ -1,58 +1,61 @@
-/*
- *
- * PROGRAM FOR DETECTING FACES FROM GIVEN IMAGE USING OPENCV 
- *
- *
- */
-
+package it.polito.teaching.cv;
 
 import org.opencv.core.Core;
-import java.lang.String;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
 
-class FaceDetector
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class FaceDetection extends Application
 {
-    public static void main(String[] args)
-    {
-
-        // For proper execution of native libraries
-        // Core.NATIVE_LIBRARY_NAME must be loaded before
-        // calling any of the opencv methods
-        System.loadLibrary("opencv");
-
-        // Face detector creation by loading source cascade xml file
-        // using CascadeClassifier.
-        // the file can be downloade from
-        // https://github.com/opencv/opencv/blob/master/data/haarcascades/
-        // haarcascade_frontalface_alt.xml
-        // and must be placed in same directory of the source java file
-        CascadeClassifier faceDetector = new CascadeClassifier();
-        faceDetector.load("/home/hawkeye/haarcascade_frontalface_alt.xml");
-
-        // Input image
-        Mat image = Imgcodecs.imread("/home/hawkeye/1.jpg");
-
-        // Detecting faces
-        MatOfRect faceDetections = new MatOfRect();
-        faceDetector.detectMultiScale(image, faceDetections);
-
-        // Creating a rectangular box showing faces detected
-        for (Rect rect : faceDetections.toArray())
-        {
-            Imgproc.rectangle(image, new Point(rect.x, rect.y),
-             new Point(rect.x + rect.width, rect.y + rect.height),
-                                           new Scalar(0, 255, 0));
-        }
-
-        // Saving the output image
-        String filename = "face1.png";
-        Imgcodecs.imwrite("/home/hawkeye/Pictures/"+filename, image);
-    }
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			// load the FXML resource
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("FaceDetection.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			// set a whitesmoke background
+			root.setStyle("-fx-background-color: whitesmoke;");
+			// create and style a scene
+			Scene scene = new Scene(root, 800, 600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// create the stage with the given title and the previously created
+			// scene
+			primaryStage.setTitle("Face Detection and Tracking");
+			primaryStage.setScene(scene);
+			// show the GUI
+			primaryStage.show();
+			
+			// init the controller
+			FaceDetectionController controller = loader.getController();
+			controller.init();
+			
+			// set the proper behavior on closing the application
+			primaryStage.setOnCloseRequest((new EventHandler<WindowEvent>() {
+				public void handle(WindowEvent we)
+				{
+					controller.setClosed();
+				}
+			}));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		// load the native OpenCV library
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		launch(args);
+	}
 }
